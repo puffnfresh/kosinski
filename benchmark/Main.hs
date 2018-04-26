e790ba8d58d78b7bac2a12ae2a52f7ba6704e28f
Brian McKenna <brian@brianmckenna.org>
2018-04-26 19:07:27 +1000

    Use compressedFile in benchmark

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,21 +1,17 @@
 module Main where
 
-import           Codec.Compression.Kosinski (compressed, decompress)
+import           Codec.Compression.Kosinski (compressedFile, decompress)
 import           Control.DeepSeq            (deepseq)
 import qualified Data.ByteString            as BS
 import           Data.Time.Clock            (diffUTCTime, getCurrentTime)
 import           Data.Word                  (Word8)
 import           System.FilePath            ((</>))
 
-decompressByteString :: BS.ByteString -> Maybe [Word8]
-decompressByteString =
-  (>>= decompress) . compressed
-
 main :: IO ()
 main = do
   -- Large file generated via KENSSharp
-  bin <- BS.readFile ("benchmark" </> "haskell-logo-png.bin")
+  Just bin <- compressedFile ("benchmark" </> "haskell-logo-png.bin")
   start <- getCurrentTime
-  deepseq (decompressByteString bin) $ return ()
+  deepseq (decompress bin) $ return ()
   end <- getCurrentTime
   putStrLn $ "Kosinski decompress took " ++ show (diffUTCTime end start)
